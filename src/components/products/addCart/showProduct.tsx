b2b61f25e0fd0650eca50bd1cf7f26464548fa25
saeed <skandaryan.m2b@gmail.com>
2024-08-23 20:12:00 +0330

    add favorites to user db

--- src/components/products/addCart/showProduct.tsx
+++ src/components/products/addCart/showProduct.tsx
@@ -7,13 +7,28 @@ import {useDispatch} from "react-redux";
 import {changePath} from "@/store/features/categorySlice";
 import ShowMoreText from "react-show-more-text";
 import {useImageSrc} from "@/hooks/src";
+import Image from "next/image";
 
-export const ShowSingleProduct = ({product}: {product: ProductType}) => {
+export const ShowSingleProduct = ({product}: { product: ProductType }) => {
     const [slideIndex, setSlideIndex] = useState(0);
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(changePath(''))
     }, []);
+
+    function imagePath({src}) {
+        return useImageSrc(src)
+    }
+
+    const [basketItem, setBasketItem] = useState({
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        discount: product.discount,
+        colors: [product.colors[0].color],
+        sizes: [product.sizes[0].size],
+        images: product.images
+    });
 
     return (
         <>
@@ -23,7 +38,7 @@ export const ShowSingleProduct = ({product}: {product: ProductType}) => {
                         <h4 className={''}>{product.title}</h4>
                         <div className={'flex flex-col'}>
                             <span>قیمت:{product.price}</span>
-                            {Number(product.discount )> 1 && <span
+                            {Number(product.discount) > 1 && <span
                                 className={'text-red-500'}> تخفیف:{Math.floor((Number(product.discount) * Number(product.price)) / 100)}</span>}
                         </div>
                         <div>
@@ -34,7 +49,9 @@ export const ShowSingleProduct = ({product}: {product: ProductType}) => {
                         <div>
                             {product?.colors?.map((color, index) => {
                                 return (
-                                    <button key={index} style={{backgroundColor: color.color}} className={'w-7 h-7'}/>
+                                    <button key={index} className={`w-10 aspect-square border p-2 ${basketItem.colors.includes(color.color) ? 'border-black' : 'border-gray-200'}`}>
+                                        <div className={'w-full h-full'} style={{backgroundColor: color.color}} ></div>
+                                    </button>
                                 )
                             })}
                         </div>
@@ -53,18 +70,18 @@ export const ShowSingleProduct = ({product}: {product: ProductType}) => {
                     </BtnPrimary>
                 </div>
                 <div className={'flex gap-3 items-end'}>
-                    <div className={'w-20'}>
+                    <div className={'w-20 aspect-product'}>
                         {product.images.map((image, index) => {
-                            let src;
-                            if (image.src.includes("via.placeholder.com")) {
-                                src = image.src
-                            } else {
-                                src = process.env.IMAGE_DIRECTORY + image.src
-                            }
                             return (
-                                <img key={index} className={'cursor-pointer aspect-[1/1.3]'}
-                                     onClick={() => setSlideIndex(index)}
-                                     src={src} alt={product.title}/>
+                                <div key={index} className={'relative'} onClick={() => setSlideIndex(index)}>
+                                    <Image fill
+                                           className={'z-0 object-cover'}
+                                           src={imagePath({src: image.src})}
+                                           alt={product.title}
+                                           sizes={'(width:100%)'}
+                                           priority
+                                    />
+                                </div>
                             )
                         })}
                     </div>
@@ -79,33 +96,49 @@ export const ShowSingleProduct = ({product}: {product: ProductType}) => {
     );
 };
 
-export const ShowCollectionProduct = ({product}: {product: ProductType}) => {
+export const ShowCollectionProduct = ({product}: { product: ProductType }) => {
     const [slideIndex, setSlideIndex] = useState(0);
-    const [showMore, setShowMore] = useState(false);
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(changePath(''))
     }, []);
 
+    function imagePath({src}) {
+        return useImageSrc(src)
+    }
+
+    const [basketItem, setBasketItem] = useState({
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        discount: product.discount,
+        colors: [product.colors[0].color],
+        sizes: [product.sizes[0].size],
+        images: product.images
+    });
+
     return (
         <>
-            <section
-                className={'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 h-full p-5 md:p-14 gap-5 mx-auto max-w-[1280px] md:max-h-[60vh]'}>
-                <div className={'col-span-1 border border-black mx-10 mb-10 flex flex-col justify-between max-h-full '}>
-                    <div className={'p-10 max-h-full scrollBarMain overflow-y-auto'}>
+            <section className={'grid grid-cols-1 sm:grid-cols-3 h-full p-14 gap-5 mx-auto max-w-[1280px]'}>
+                <div className={'col-span-1 border border-black mx-10 mb-10 flex flex-col justify-between max-'}>
+                    <div className={'p-10 max-h-full'}>
                         <h4 className={''}>{product.title}</h4>
                         <div className={'flex flex-col'}>
                             <span>قیمت:{product.price}</span>
-                            {Number(product.discount )> 1 && <span
+                            {Number(product.discount) > 1 && <span
                                 className={'text-red-500'}> تخفیف:{Math.floor((Number(product.discount) * Number(product.price)) / 100)}</span>}
                         </div>
-                        <div dangerouslySetInnerHTML={{__html: product.maintenance}}></div>
+                        <div>
+                            {product.description}
+                        </div>
                     </div>
                     <div>
                         <div>
                             {product?.colors?.map((color, index) => {
                                 return (
-                                    <button key={index} style={{backgroundColor: color.color}} className={'w-7 h-7'}/>
+                                    <button key={index} className={`w-10 aspect-square border p-2 ${basketItem.colors.includes(color.color) ? 'border-black' : 'border-gray-200'}`}>
+                                        <div className={'w-full h-full'} style={{backgroundColor: color.color}} ></div>
+                                    </button>
                                 )
                             })}
                         </div>
@@ -124,30 +157,27 @@ export const ShowCollectionProduct = ({product}: {product: ProductType}) => {
                     </BtnPrimary>
                 </div>
                 <div className={'flex gap-3 items-end'}>
-                    <div className={'w-20'}>
+                    <div className={'w-20 aspect-product'}>
                         {product.images.map((image, index) => {
                             return (
-                                <img key={index} className={'cursor-pointer aspect-[1/1.3]'}
-                                     onClick={() => setSlideIndex(index)}
-                                     src={useImageSrc(image.src)} alt={product.title}/>
+                                <div key={index} className={'relative'} onClick={() => setSlideIndex(index)}>
+                                    <Image fill
+                                           className={'z-0 object-cover'}
+                                           src={imagePath({src: image.src})}
+                                           alt={product.title}
+                                           sizes={'(width:100%)'}
+                                           priority
+                                    />
+                                </div>
                             )
                         })}
                     </div>
                     <ProductSwiper slideIndex={slideIndex} classes={'col-span-2 aspect-[1/1.2] mx-5'}
                                    images={product.images}/>
                 </div>
-                <ShowMoreText
-                    lines={3}
-                    more="مشاهده بیشر"
-                    less="مشاهده کمتر"
-                    className="scrollBarMain overscroll-x-auto border border-black self-end p-10 scrollBar overflow-y-auto max-h-full"
-                    anchorClass="show-more-less-clickable"
-                    expanded={false}
-                    width={280}
-                    truncatedEndingComponent={"... "}
-                >
-                    <div dangerouslySetInnerHTML={{__html: product.maintenance}}></div>
-                </ShowMoreText>
+                <div className={'border border-black self-end p-10 '}>
+                    {product.maintenance}
+                </div>
             </section>
         </>
     );
